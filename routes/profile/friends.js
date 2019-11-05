dfac864f9353890cb3b3f6a3e8a9dc6ccef61ad1
yuxiu198 <xiuyutiffany@gmail.com>
2019-11-04 19:39:05 -0800

    update friends

--- routes/profile/friends.js
+++ routes/profile/friends.js
@@ -1,15 +1,21 @@
+/* Node.js web application framework with a set of features of the Friends request page */
 var express = require('express');
 var router = express.Router();
 var connection = require('../database');
+
+// respond "Friends API is working properly" when the GET request is made to the Friends page
 router.get('/', function(req, res, next) {
     res.send('Friends API is working properly');
 });
 
+// handle the list friends request
+// req: get the request
+// res: send the response
 router.get('/list/:id', async (req, res) =>{
     let id =  req.params.id;
 //    let queryString = `select f1.* from FriendsListAndRequest f1 inner join FriendsListAndRequest f2 on f1.user1 = f2.user2 and f1.user1 = f2.user2;`
     let queryString = `select f1.*, tb.firstname, tb.surname from FriendsListAndRequest f1 inner join 
-FriendsListAndRequest f2 on f1.user1 = f2.user2 and f1.user2 = f2.user1 and f1.user1=23 
+FriendsListAndRequest f2 on f1.user1 = f2.user2 and f1.user2 = f2.user1 and f1.user1=${id}
 JOIN user tb ON f1.user2 = tb.iduser`;
     connection.query(queryString,
         function(error, results, fields) {
@@ -17,27 +23,32 @@ JOIN user tb ON f1.user2 = tb.iduser`;
               console.log(error);
           }
           else{
-            console.log(results[0]);
-    
-          res.json(results)};
+              // send a json response with json support
+              res.json(results)
+              res.render('The friends list: ');
+              console.log(results[0]);
+          };
         }
       );
 });
 
+// handle the request friends request
 router.get('/request/:id', async (req, res) =>{
     let id =  req.params.id;
     let queryString = `SELECT f1.* from FriendsListAndRequest f1 WHERE f1.user1 != 23 and f1.user1 
 NOT IN(select f1.user2 from FriendsListAndRequest f1 INNER JOIN FriendsListAndRequest f2 on f1.user1 = f2.user2 and 
-f1.user2 = f2.user1 and f1.user1=23);`;
+f1.user2 = f2.user1 and f1.user1=${id});`;
     connection.query(queryString,
         function(error, results, fields) {
             if (error){
                 console.log(error);
             }
             else{
+                res.json(results)
+                res.render('The friends request list');
                 console.log(results[0]);
+            };
 
-                res.json(results)};
         }
     );
 });
