b1397c726b975906acf3be47ff18b1cdfe7386bd
yuxiu198 <xiuyutiffany@gmail.com>
2019-11-12 21:28:14 -0800

    create

--- routes/profile/friends.js
+++ routes/profile/friends.js
@@ -49,30 +49,30 @@ router.get('/request/:id', async(req, res) =>{
         }
     );
 });
-// router.post('/request/create', async(req, res) =>{
-//     let id =  req.body.user1;
-//     let id2 = req.body.user2;
-//     let queryString = `INSERT INTO FriendsListAndRequest(user1, user2) VALUES(${id}, ${id2})`;
-//     connection.query(queryString,
-//         function(error, results, fields) {
-//             if (error){
-//                 console.log("ERROR", error);
-//                 return res.status(400).json({
-//                     error: true,
-//                     message: "cannot send friend request"
-//                 });
-//             }
-//             else{
-//                 res.json(results);
-//                 return res.status(200).json({
-//                     error: false,
-//                     message: "message sends to the user"
-//                 });
-//             };
-//
-//         }
-//     );
-// });
+router.post('/request/create', async(req, res) =>{
+    let id =  req.body.user1;
+    let id2 = req.body.user2;
+    let queryString = `INSERT INTO FriendsListAndRequest(user1, user2) VALUES(${id}, ${id2})`;
+    connection.query(queryString,
+        function(error, results, fields) {
+            if (error){
+                console.log("ERROR", error);
+                return res.status(400).json({
+                    error: true,
+                    message: "cannot send friend request"
+                });
+            }
+            else{
+                res.json(results);
+                return res.status(200).json({
+                    error: false,
+                    message: "message sends to the user"
+                });
+            };
+
+        }
+    );
+});
 // router.delete('/delete/:id', async(req, res) =>{
 //     let id = req.params.id;
 //     let queryString = `DELETE FROM FriendsListAndRequest
