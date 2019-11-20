e4bb868911813e6c84ea3355c41e474696da39f1
jvan1997 <jonathanvan.1997@gmail.com>
2019-11-19 22:47:31 -0800

    database location was off

--- routes/postrelated/reservation.js
+++ routes/postrelated/reservation.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var connection = require('./database');
+var connection = require('../database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
