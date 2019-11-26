920b37b1cfec9026d802e71f22bb12cbe4985451
colemckinnon <colemckinnon.school@gmail.com>
2019-11-25 22:14:08 -0800

    Starting login routes.

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('./database');
+
+
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
 });
