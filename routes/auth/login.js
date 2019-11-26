278fe8b3639df803be24e9f3da6ac6d7814df7d7
colemckinnon <colemckinnon.school@gmail.com>
2019-11-25 23:37:13 -0800

    Added test route

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -7,4 +7,29 @@ router.get('/', function(req, res, next) {
     res.send('Post api is working properly');
 });
 
+router.get('/getUser/:id', function(req,res,next){
+      let id =  req.params.id;
+      queryString = `SELECT * FROM user WEHRE userID=${id};`;
+      connection.query(
+        queryString,
+        function(error, results, fields) {
+          if (error){
+              console.log(error);
+            return res.status(400).json({
+                error: true,
+                message: "Error getting the posts"
+              }); 
+          }
+          else{
+            return res.status(200).json({
+                error:false,
+                message: "Successfully returned post",
+                data: results
+              }); 
+            }
+        }
+      );
+});
+
+
 module.exports = router;
