861d6b9a80fe7683c03f46b96827d561ab24f891
jvan1997 <jonathanvan.1997@gmail.com>
2019-12-01 22:52:12 -0800

    wrong table name

--- routes/postrelated/reservation.js
+++ routes/postrelated/reservation.js
@@ -49,7 +49,7 @@ router.post('/create', async function(req, res) {
     seller = req.body.seller;
     id = req.body.postID;
     date = req.body.date;
-    let queryString = `INSERT INTO Hold (\`buyer\`, \`timer\`, \`seller\`,\`postID\`) VALUES(${buyer},"${date}",${seller},${id});`;
+    let queryString = `INSERT INTO Holds (\`buyer\`, \`timer\`, \`seller\`,\`postID\`) VALUES(${buyer},"${date}",${seller},${id});`;
     let requestMaker = `INSERT INTO FriendsListAndRequest(user1, user2) VALUES(${buyer}, ${seller}) SELECT '${buyer}', '${seller}'
     WHERE NOT EXISTS
         (SELECT * FROM FriendsListAndRequest WHERE user1 = ${buyer} AND user2 =${seller});`;
