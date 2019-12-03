ffe1a90b2977e1dd6615ed57eb90993b031dd594
colemckinnon <colemckinnon.school@gmail.com>
2019-12-02 20:29:11 -0800

    Starting progress on signup route

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -3,31 +3,32 @@ var router = express.Router();
 var connection = require('../database');
 
 router.get('/', function(req, res, next) {
-    res.send('Post api is working properly');
+    res.send('Signup api is working properly');
 });
 
 router.post('/submit', async function(req,res){
-      email =  req.body.email;
-      password =  req.body.password;
-    
+    let email =  req.body.email;
+    let password =  req.body.password;
+    let firstname = req.body.firstname;
+    let surname = req.body.surname;
+    let schoolid = req.body.schoolid;
+
       /*
-    1. Take a username and 'password'
-    2. Check if username matches, than password (or both at the same time)
-    3. If 
-    */
-      queryString = `SELECT schoolid, firstname, surname, email FROM user WHERE email="${email}" AND password="${password}";`;
-      connection.query(
-        queryString,
+      1. Check if email is already in system
+      */
+     let queryString = `SELECT email FROM user WHERE email="${email}";`;
+     connection.query(
+        queryString,
         function(error, results, fields) {
           if (error){
               console.log(error);
                 return res.status(400).json({
                     error: true,
-                    message: "Error getting the posts"
+                    message: "Error checking emails"
               }); 
           }
           else{
-                console.log("Success!!")  
+                console.log("Success getting emails")  
                 res.json(results);
             }
         }
