57c1711790723a72f15b6a7dcd62fdea827e8192
colemckinnon <colemckinnon.school@gmail.com>
2019-12-02 22:50:47 -0800

    Added require bcrypt

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../database');
+const bcrypt = require('bcryptjs')
 
 router.get('/', function (req, res, next) {
     res.send('Signup api is working properly');
