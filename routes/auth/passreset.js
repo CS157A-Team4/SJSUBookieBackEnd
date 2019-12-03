e75eee25713b6547d39824d9ea737e22ecd4e6e1
colemckinnon <colemckinnon.school@gmail.com>
2019-12-03 05:16:45 -0800

    Cleanup. Email is sent to hardcoded address.

--- routes/auth/passreset.js
+++ routes/auth/passreset.js
@@ -45,7 +45,7 @@ router.post('/', async function(req, res){
         to: 'colemckinnon.school@gmail.com',
         subject: 'Bookie Password Reset',
         text: `You recently requested to change the password on your SJSU Bookie account. 
-               Enter 5 digit code below to begin the reset process.\nCode: ${code}`
+        Enter 5 digit code below to begin the reset process.\n\nCode: ${code}`
       };
       
       transporter.sendMail(mailOptions, function(error, info){
