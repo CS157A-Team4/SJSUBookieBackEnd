00eabaa6ddae6963ace2bc288a2e5e6acfe0793b
yuxiu198 <xiuyutiffany@gmail.com>
2019-12-02 23:19:30 -0800

    Update posts.js

--- routes/profile/posts.js
+++ routes/profile/posts.js
@@ -7,9 +7,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/getAll/:id', function(req,res,next){
     let id =  req.params.id;
-    queryPosts = `SELECT * FROM Post JOIN PostImage ON PostImage.imageID = Post.imageId JOIN user ON user.iduser = Post.seller LEFT JOIN (SELECT Holds.postID, Holds.hold FROM Holds WHERE Holds.timer >= CURDATE()) tb4 ON tb4.postID =Post.postID WHERE seller=${id};`;
-    queryHolds= `SELECT * FROM Holds JOIN Post ON Holds.postID = Post.postID JOIN PostImage ON PostImage.imageID = Post.imageId JOIN user ON user.iduser = Post.seller LEFT JOIN (SELECT Holds.postID, Holds.hold FROM Holds WHERE Holds.timer >= CURDATE()) tb4 ON tb4.postID =Post.postID WHERE buyer=${id} AND timer >= CURDATE();`;
-    querySavedPosts = `SELECT *, SavedPost.imageID AS image FROM SavedPost JOIN Post ON Post.postID = SavedPost.postid JOIN user ON user.iduser = Post.seller LEFT JOIN (SELECT Holds.postID, Holds.hold FROM Holds WHERE Holds.timer >= CURDATE()) tb4 ON tb4.postID =Post.postID WHERE SavedPost.userID=${id};`;
+    queryPosts = `SELECT *, COALESCE(tb4.hold,0) AS 'hold' FROM Post JOIN PostImage ON PostImage.imageID = Post.imageId JOIN user ON user.iduser = Post.seller LEFT JOIN (SELECT Holds.postID, Holds.hold FROM Holds WHERE Holds.timer >= CURDATE()) tb4 ON tb4.postID =Post.postID WHERE seller=${id};`;
+    queryHolds= `SELECT *, COALESCE(tb4.hold,0) AS 'hold'FROM Holds JOIN Post ON Holds.postID = Post.postID JOIN PostImage ON PostImage.imageID = Post.imageId JOIN user ON user.iduser = Post.seller LEFT JOIN (SELECT Holds.postID, Holds.hold FROM Holds WHERE Holds.timer >= CURDATE()) tb4 ON tb4.postID =Post.postID WHERE buyer=${id} AND timer >= CURDATE();`;
+    querySavedPosts = `SELECT *, SavedPost.imageID AS image, COALESCE(tb4.hold,0) AS 'hold' FROM SavedPost JOIN Post ON Post.postID = SavedPost.postid JOIN user ON user.iduser = Post.seller LEFT JOIN (SELECT Holds.postID, Holds.hold FROM Holds WHERE Holds.timer >= CURDATE()) tb4 ON tb4.postID =Post.postID WHERE SavedPost.userID=${id};`;
 
 
     connection.query( queryPosts + queryHolds + querySavedPosts,
