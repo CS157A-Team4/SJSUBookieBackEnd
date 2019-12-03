8990f077fbe09f284df6a051a0e27d73067bfe91
colemckinnon <colemckinnon.school@gmail.com>
2019-12-02 21:13:47 -0800

    Testing a second query with await

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -2,47 +2,62 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../database');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.send('Signup api is working properly');
 });
 
-router.post('/submit', async function(req,res){
-    let email =  req.body.email;
-    let password =  req.body.password;
+router.post('/submit', async function (req, res) {
+    let email = req.body.email;
+    let password = req.body.password;
     let firstname = req.body.firstname;
     let surname = req.body.surname;
     let schoolid = req.body.schoolid;
 
-      /*
-      1. Check if email is already in system
-      */
-     let queryString = `SELECT email FROM user WHERE email="${email}";`;
-     await connection.query(
-        queryString, (error, results, fields) => {
-            if (error){
-              console.log(error);
-                return res.status(400).json({
-                    error: true,
-                    message: "Error checking emails"
-              }); 
-            }
-            else{
-                console.log(typeof results)
-                console.log("Success getting emails")
-                console.log(results)
-                
-                if(results.length > 0){
-                    res.json({
-                        message: "This email is already in the system!"
-                    })
-                }else{
-                    res.json({
-                        message: "This email does not yet exist. Good job!"
-                    })
-                }   
+    /*
+          1. Check if email is already in system
+    */
+    let queryString = `SELECT email FROM user WHERE email="${email}";`;
+    await connection.query(queryString, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+            return res.status(400).json({
+                error: true,
+                message: "Error checking emails"
+            });
+        }
+        else {
+            console.log(typeof results)
+            console.log("Success getting emails")
+            console.log(results)
 
-            }
-      });
+            if (results.length > 0) {
+                res.json({
+                    message: "This email is already in the system!"
+                })
+            } else {
+                res.json({
+                    message: "This email does not yet exist. Good job!"
+                })
+            }
+
+        }
+    });
+
+    queryString = `SELECT * FROM user WHERE email=CB@sjsu.edu;`
+    await connection.query(
+        queryString, (error, results, fields) => {
+            if (error) {
+                console.log(error);
+                return res.status(400).json({
+                    error: true,
+                    message: "Error checking emails"
+                });
+            }
+            else {
+                console.log("Success doing second query!")
+                console.log(results)
+            }
+        });
 
 });
 
